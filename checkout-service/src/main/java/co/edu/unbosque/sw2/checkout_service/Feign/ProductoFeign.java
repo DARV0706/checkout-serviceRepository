23699467119806500670d2b3de6ac8be7b9d04ce
ManuelTerreros <114838362+ManuelTerreros@users.noreply.github.com>
2024-10-29 17:14:05 -0500

    Cambio errores

--- checkout-service/src/main/java/co/edu/unbosque/sw2/checkout_service/Feign/ProductoFeign.java
+++ checkout-service/src/main/java/co/edu/unbosque/sw2/checkout_service/Feign/ProductoFeign.java
@@ -5,12 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="products-service", url = "http://localhost:8080")
-//@RequestMapping("/productos")
+@FeignClient(name="products-service")
 public interface ProductoFeign {
     @GetMapping("productos/lista")
     List<ProductDTO> getProductsById(@RequestParam List<Integer>ids);
