a77f8df708cf8cc7a4fcd8d115aaee3f64cb4b09
ManuelTerreros <114838362+ManuelTerreros@users.noreply.github.com>
2024-11-02 19:17:45 -0500

    Cambios

--- checkout-service/src/main/java/co/edu/unbosque/sw2/checkout_service/Feign/ProductoFeign.java
+++ checkout-service/src/main/java/co/edu/unbosque/sw2/checkout_service/Feign/ProductoFeign.java
@@ -1,17 +1,17 @@
 package co.edu.unbosque.sw2.checkout_service.Feign;
 
+import co.edu.unbosque.sw2.checkout_service.Configuration.LoadBalancingConfiguration;
 import co.edu.unbosque.sw2.checkout_service.DTO.ProductDTO;
 import co.edu.unbosque.sw2.checkout_service.DTO.ProductoResponseDTO;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(name="products-service")
+@LoadBalancerClient(value="products-service", configuration= LoadBalancingConfiguration.class )
 public interface ProductoFeign {
     @GetMapping("productos/lista")
     ProductoResponseDTO getProductsById(@RequestParam List<Integer> ids);
